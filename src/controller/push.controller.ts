e2ea69461a9f5dc02daf30b9f148fbfe8c48d1db
M-Mateen-Khokher <muhammadmateenhaider83@gmail.com>
2026-09-19 12:44:00 +0500

    add some features

--- src/controller/push.controller.ts
+++ src/controller/push.controller.ts
@@ -156,7 +156,7 @@ export const sendTestPush =
         title: "Test Notification 🔔",
         message:
           "Web Push notification is working successfully!",
-        url: "/",
+        url: "/admin",
       });
 
       return res.status(200).json({
